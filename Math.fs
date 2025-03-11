163d2f9adc3e8cecf03c81050aaaf0735b74de71
BuildTools <unconfigured@null.spigotmc.org>
2025-03-11 10:07:25 +0900

    Quiz2

--- Math.fs
+++ Math.fs
@@ -5,5 +5,16 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
+let f n =
+  if n % 2UL = 0UL then n / 2UL
+  else 3UL * n + 1UL
+
 let collatz n =
-  0
+  let rec iter cnt n  = 
+    if n = 1UL then
+      cnt
+    else
+      let result = f n
+      //printfn "%d" result
+      iter (cnt + 1) result
+  iter 0 (uint64 n)
